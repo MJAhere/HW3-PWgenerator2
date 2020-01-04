c95428d35f1dd4ec184c49344ac5ae86855d61a2
Michael Aiello <mjahere023@gmail.com>
2020-01-04 14:44:59 -0700

    Cleaned up JS, fixed reoccuring bug and finished styling page.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Charecters to be used in the password. User will have ability to select which charecters.
-var specChar = [
+// multiple arrays for each charecter type
+var specialCharacters = [
     "@",
     "%",
     "+",
@@ -23,132 +23,193 @@ var specChar = [
     "-",
     "_",
     "."
-];
-
-var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var lowLetters = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-//Here the user will be able to decide length and which charecters he/she would like to use in their password.
-
-function getPasswordChoices() {
+  ];
+  
+  
+  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  
+  
+  var lowerCasedCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  
+  
+  var upperCasedCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  
+  // Function for user prompts
+  function getPasswordOptions() {
+    
     var length = parseInt(prompt(
-        "How long would you prefer your password to be?"
+      "How many characters would you like your password to contain?"
     ));
-
+  
+    
     if (isNaN(length) === true) {
-        alert("Password length must be a number");
-        return;
-    };
-
-    if (length < 8) {
-        alert("Password length needs to be atleast 8 charecters");
-        return;
-    };
-
-    if (length > 128) {
-        alert("Password length needs to be less than 129 charecters");
-        return;
-    };
-
-    var hasSpecChar = confirm(
-        "Click ok to use numbers."
-    );
-
-    var hasCapLetters = confirm(
-        "Click ok to use lowercase letters."
-    );
-
-    var hasNumChar = confirm(
-        "Click ok to use uppercase letters."
-    );
-
-    var hasLowLetters = confirm(
-        "Click ok to use special charecters."
-    );
-
-    //This is where the user will be kicked for not properly choosing.
-
-    if (
-        hasSpecChar === false &&
-        hasCapLetters === false &&
-        hasNumChar === false &&
-        hasLowLetters === false
-
-    ) {
-        alert("You have to select atleast one charecter set.")
-        return;
+      alert("Password length must be provided as a number");
+      return;
     }
-
-    //This is for storing the users choices,
-
-    var passwordChoices = {
-        length: length,
-        hasSpecChar: hasSpecChar,
-        hasCapLetters: hasCapLetters,
-        hasNumChar: hasNumChar,
-        hasLowLetters: hasLowLetters
+  
+    
+    if (length < 8) {
+      alert("Password length must be at least 8 characters");
+      return;
+    }
+  
+    if (length > 128) {
+      alert("Password length must less than 129 characters");
+      return;
+    }
+  
+    
+    var hasSpecialCharacters = confirm(
+      "Click OK to confirm including special characters."
+    );
+  
+    var hasNumericCharacters = confirm(
+      "Click OK to confirm including numeric characters."
+    );
+  
+    
+    var hasLowerCasedCharacters = confirm(
+      "Click OK to confirm including lowercase characters."
+    );
+  
+    
+    var hasUpperCasedCharacters = confirm(
+      "Click OK to confirm including uppercase characters."
+    );
+  
+    // Statement that will boot user if no charecters chosen
+    if (
+      hasSpecialCharacters === false &&
+      hasNumericCharacters === false &&
+      hasLowerCasedCharacters === false &&
+      hasUpperCasedCharacters === false
+    ) {
+      alert("Must select at least one character type");
+      return;
+    }
+  
+    
+    var passwordOptions = {
+      length: length,
+      hasSpecialCharacters: hasSpecialCharacters,
+      hasNumericCharacters: hasNumericCharacters,
+      hasLowerCasedCharacters: hasLowerCasedCharacters,
+      hasUpperCasedCharacters: hasUpperCasedCharacters
     };
-
-    return passwordChoices;
-
-};
-
-//This is the function to gen a random pw. 
-
-function genRandom(arr) {
+  
+    return passwordOptions;
+  }
+  
+  // Function to randomize arrays.
+  function getRandom(arr) {
     var randIndex = Math.floor(Math.random() * arr.length);
     var randElement = arr[randIndex];
   
     return randElement;
   }
   
-//Password will be generated using the users input.
-
-function generatePassword() {
-    var options = getPasswordChoices();
-
+  // Function to generate password with user input
+  function generatePassword() {
+    var options = getPasswordOptions();
+   
     var result = [];
-
-    var possibleChoices = [];
-
-    var setChoices = [];
-
-    if (options.hasSpecChar){
-        possibleChoices = possibleChoices.concat(specChar);
-        setChoices.push(genRandom(specChar));
+  
+   
+    var possibleCharacters = [];
+  
+    
+    var guaranteedCharacters = [];
+  
+    
+    if (options.hasSpecialCharacters) {
+      possibleCharacters = possibleCharacters.concat(specialCharacters);
+      guaranteedCharacters.push(getRandom(specialCharacters));
     }
-
-    if (options.hasCapLetters) {
-        possibleChoices = possibleChoices.concat(capLetters);
-        setChoices.push(genRandom(capLetters));
+  
+    if (options.hasNumericCharacters) {
+      possibleCharacters = possibleCharacters.concat(numericCharacters);
+      guaranteedCharacters.push(getRandom(numericCharacters));
     }
-
-    if (options.hasNumChar) {
-        possibleChoices = possibleChoices.concat(numChar);
-        setChoices.push(genRandom(numChar));
+  
+    if (options.hasLowerCasedCharacters) {
+      possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+      guaranteedCharacters.push(getRandom(lowerCasedCharacters));
     }
-
-    if (options.hasCapLetters) {
-        possibleChoices = possibleChoices.concat(lowLetters)
-        setChoices.push(genRandom(lowLetters));
+  
+    if (options.hasUpperCasedCharacters) {
+      possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+      guaranteedCharacters.push(getRandom(upperCasedCharacters));
     }
-
-    for(var i = 0; i < options.length; i++) {
-        var possibleChoices = genRandom(possibleChoices);
-
-        result.push(possibleChoices);
+  
+    
+    for (var i = 0; i < options.length; i++) {
+      var possibleCharacter = getRandom(possibleCharacters);
+  
+      result.push(possibleCharacter);
     }
-
-    for(var i = 0; i < setChoices.length; i++) {
-        result[i] = setChoices[i];
+  
+    
+    for (var i = 0; i < guaranteedCharacters.length; i++) {
+      result[i] = guaranteedCharacters[i];
     }
-
+  
+   //to string
     return result.join("");
- 
-}
+  }
 //copy and gen button
 var copyBtn = document.querySelector("#copy");
 var generateBtn = document.querySelector("#generate");
