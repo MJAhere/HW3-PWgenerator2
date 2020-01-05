442a637439f718c75d6f601b265cb82a2d139391
Michael Aiello <mjahere023@gmail.com>
2020-01-05 06:00:43 -0700

    Formatted to make easier to read.

--- script.js
+++ script.js
@@ -26,7 +26,18 @@ var specialCharacters = [
   ];
   
   
-  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var numericCharacters = [
+    "0",
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5", 
+    "6", 
+    "7", 
+    "8", 
+    "9"];
+    
   
   
   var lowerCasedCharacters = [
